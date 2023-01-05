147d31273509719cfb333e637066a96be967d1ea
Felix Feng <fzh_tj@126.com>
2023-01-05 11:25:54 +0800

    update

---
+++ module_salary_145.py
@@ -0,0 +1,11 @@
+'''
+本模块用于计算公司员工的工资
+'''
+company="微软"
+def yearSalary(monthSalary):
+    '''根据传入的月薪，计算出年薪'''
+    pass
+
+def daySalary(monthSalary):
+    '''根据传入的月薪，计算出每天的薪资'''
+    pass
---
+++ test_145.py
@@ -0,0 +1,3 @@
+import module_salary_145
+print(module_salary_145.__doc__)
+print(module_salary_145.daySalary.__doc__)
